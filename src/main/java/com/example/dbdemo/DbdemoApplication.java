795a81699820c72a680a061a863f7b479ed07b69
harsh <harshapotluri92@gmail.com>
2025-10-14 22:57:15 -0400

    1st commit

---
+++ src/main/java/com/example/dbdemo/DbdemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.dbdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbdemoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DbdemoApplication.class, args);
+		System.out.println("Main Page");
+	}
+
+}
